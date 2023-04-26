1953020308c625057804cadb7999451a4a0cae7b
Levi Lesches <levilesches@gmail.com>
2023-04-26 16:53:31 -0400

    Improved gen_python

--- gen-python.py
+++ gen-python.py
@@ -1,2 +1,8 @@
+from argparse import ArgumentParser
 import os
-os.system("protoc --python_out=src/generated -I Protobuf Protobuf/*.proto")
+
+parser = ArgumentParser()
+parser.add_argument("--proto", "-p", type=str, required=True, help="Proto file name, NOT including .proto extension")
+args = parser.parse_args()
+
+os.system(f"protoc --python_out=src/generated -I Protobuf Protobuf/{args.proto}.proto")
